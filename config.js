0eb7a1d3be97d236581880756227f4b1d023af4a
Xoul ser <131429508+sabariy@users.noreply.github.com>
2024-09-08 20:50:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://helloxoul:Sabari777@cluster0.kf4mx2d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   SESSION_NAME: process.env.SESSION_NAME || "Bot~"
 };
