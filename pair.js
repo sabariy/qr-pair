20f66a88861d95f091c6d8f6d1494df089cbb948
Xoul ser <131429508+sabariy@users.noreply.github.com>
2024-09-09 20:07:35 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,88 +1,236 @@
-const axios = require('axios');
-const { MONGODB_URL, SESSION_NAME } = require('./config');
-const { makeid } = require('./id');
-const express = require('express');
-const fs = require('fs');
-let router = express.Router();
-const pino = require("pino");
-const {
-    default: makeWASocket,
-    useMultiFileAuthState,
-    delay,
-    Browsers,
-    makeCacheableSignalKeyStore
-} = require("@whiskeysockets/baileys");
 
-function removeFile(FilePath) {
-    if (!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true });
-};
+const makeWASocket = require("@whiskeysockets/baileys").default
 
-router.get('/', async (req, res) => {
-    const id = makeid();
-    let num = req.query.number;
+const qrcode = require("qrcode-terminal")
 
-    async function getPaire() {
-        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
-        try {
-            let session = makeWASocket({
-                auth: {
-                    creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
-                },
-                printQRInTerminal: false,
-                logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: Browsers.macOS("Safari"),
-             });
+const fs = require('fs')
 
-            if (!session.authState.creds.registered) {
-                await delay(1500);
-                num = num.replace(/[^0-9]/g, '');
-                const code = await session.requestPairingCode(num);
-                if (!res.headersSent) {
-                    await res.send({ code });
-                }
-            }
+const pino = require('pino')
 
-            session.ev.on('creds.update', saveCreds);
+const { delay, useMultiFileAuthState, BufferJSON, fetchLatestBaileysVersion, PHONENUMBER_MCC, DisconnectReason, makeInMemoryStore, jidNormalizedUser, makeCacheableSignalKeyStore } = require("@whiskeysockets/baileys")
 
-            session.ev.on("connection.update", async (s) => {
-                const { connection, lastDisconnect } = s;
+const Pino = require("pino")
 
-                if (connection == "open") {
-                    await delay(5000);
-                    await delay(5000);
+const NodeCache = require("node-cache")
 
-                    const jsonData = await fs.promises.readFile(`${__dirname}/temp/${id}/creds.json`, 'utf-8');
-                    const { data } = await axios.post('https://api.lokiser.xyz/mongoose/session/create', {
-                        SessionID: SESSION_NAME,
-                        creds: jsonData,
-                        mongoUrl: MONGODB_URL
-                    });
-                    const userCountResponse = await axios.post('https://api.lokiser.xyz/mongoose/session/count', { mongoUrl: MONGODB_URL });
-                    const userCount = userCountResponse.data.count;
-                    
-                    await session.sendMessage(session.user.id, { text: ` *𝘛𝘩𝘢𝘯𝘬 𝘠𝘰𝘶 𝘍𝘰𝘳 𝘊𝘩𝘰𝘰𝘴𝘪𝘯𝘨 𝘍𝘦𝘯𝘪𝘹 𝘔𝘥\n\n 𝘋𝘰𝘯'𝘵 𝘚𝘩𝘢𝘳𝘦 𝘠𝘰𝘶 𝘚𝘦𝘴𝘴𝘪𝘰𝘯 𝘐𝘥 𝘞𝘪𝘵𝘩 𝘌𝘯𝘺𝘰𝘯𝘦\n\n *Total Scan :* ${userCount}` });
-                    await session.sendMessage(session.user.id, { text: data.data });
+const chalk = require("chalk")
 
-                    await delay(100);
-                    await session.ws.close();
-                    return await removeFile('./temp/' + id);
-                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-                    await delay(10000);
-                    getPaire();
-                }
-            });
-        } catch (err) {
-            console.log("service restated");
-            await removeFile('./temp/' + id);
-            if (!res.headersSent) {
-                await res.send({ code: "Service Unavailable" });
-            }
+const readline = require("readline")
+
+const { parsePhoneNumber } = require("libphonenumber-js")
+
+
+
+
+
+let phoneNumber = "+919633605648"
+
+
+
+const pairingCode = !!phoneNumber || process.argv.includes("--pairing-code")
+
+const useMobile = process.argv.includes("--mobile")
+
+
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+
+const question = (text) => new Promise((resolve) => rl.question(text, resolve))
+
+
+
+
+
+  async function qr() {
+
+//------------------------------------------------------
+
+let { version, isLatest } = await fetchLatestBaileysVersion()
+
+const {  state, saveCreds } =await useMultiFileAuthState(`./sessions`)
+
+    const msgRetryCounterCache = new NodeCache() // for retry message, "waiting message"
+
+    const XeonBotInc = makeWASocket({
+
+        logger: pino({ level: 'silent' }),
+
+        printQRInTerminal: !pairingCode, // popping up QR in terminal log
+
+      mobile: useMobile, // mobile api (prone to bans)
+
+      browser: ['Chrome (Linux)', '', ''], // for this issues https://github.com/WhiskeySockets/Baileys/issues/328
+
+     auth: {
+
+         creds: state.creds,
+
+         keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
+
+      },
+
+      browser: ['Chrome (Linux)', '', ''], // for this issues https://github.com/WhiskeySockets/Baileys/issues/328
+
+      markOnlineOnConnect: true, // set false for offline
+
+      generateHighQualityLinkPreview: true, // make high preview link
+
+      getMessage: async (key) => {
+
+         let jid = jidNormalizedUser(key.remoteJid)
+
+         let msg = await store.loadMessage(jid, key.id)
+
+
+
+         return msg?.message || ""
+
+      },
+
+      msgRetryCounterCache, // Resolve waiting messages
+
+      defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
+
+   })
+
+
+
+
+
+    // login use pairing code
+
+   // source code https://github.com/WhiskeySockets/Baileys/blob/master/Example/example.ts#L61
+
+   if (pairingCode && !XeonBotInc.authState.creds.registered) {
+
+      if (useMobile) throw new Error('Cannot use pairing code with mobile api')
+
+
+
+      let phoneNumber
+
+      if (!!phoneNumber) {
+
+         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+
+
+
+         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
+
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +919633605648")))
+
+            process.exit(0)
+
+         }
+
+      } else {
+
+         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +919633605648 : `)))
+
+         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+
+
+
+         // Ask again when entering the wrong number
+
+         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
+
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +919633605648")))
+
+
+
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number 😍\nFor example: +919633605648 : `)))
+
+            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+
+            rl.close()
+
+         }
+
+      }
+
+
+
+      setTimeout(async () => {
+
+         let code = await XeonBotInc.requestPairingCode(phoneNumber)
+
+         code = code?.match(/.{1,4}/g)?.join("-") || code
+
+         console.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
+
+      }, 3000)
+
+   }
+
+//------------------------------------------------------
+
+    XeonBotInc.ev.on("connection.update",async  (s) => {
+
+        const { connection, lastDisconnect } = s
+
+        if (connection == "open") {
+
+            await delay(1000 * 10)
+
+            await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `𝘛𝘩𝘢𝘯𝘬 𝘠𝘰𝘶 𝘍𝘰𝘳 𝘊𝘩𝘰𝘰𝘴𝘪𝘯𝘨 𝘍𝘦𝘯𝘪𝘹 𝘔𝘥\n\n 𝘋𝘰𝘯'𝘵 𝘚𝘩𝘢𝘳𝘦 𝘠𝘰𝘶 𝘚𝘦𝘴𝘴𝘪𝘰𝘯 𝘐𝘥 𝘞𝘪𝘵𝘩 𝘌𝘯𝘺𝘰𝘯𝘦` });
+
+            let sessionXeon = fs.readFileSync('./sessions/creds.json');
+
+            await delay(1000 * 2) 
+
+             const xeonses = await  XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` })
+
+             await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `𝘛𝘩𝘢𝘯𝘬 𝘠𝘰𝘶 𝘍𝘰𝘳 𝘊𝘩𝘰𝘰𝘴𝘪𝘯𝘨 𝘍𝘦𝘯𝘪𝘹 𝘔𝘥\n\n 𝘋𝘰𝘯'𝘵 𝘚𝘩𝘢𝘳𝘦 𝘠𝘰𝘶 𝘚𝘦𝘴𝘴𝘪𝘰𝘯 𝘐𝘥 𝘞𝘪𝘵𝘩 𝘌𝘯𝘺𝘰𝘯𝘦` }, {quoted: xeonses});
+
+              await delay(1000 * 2) 
+
+              process.exit(0)
+
         }
-    }
 
-    return await getPaire();
-});
+        if (
 
-module.exports = router;
+            connection === "close" &&
+
+            lastDisconnect &&
+
+            lastDisconnect.error &&
+
+            lastDisconnect.error.output.statusCode != 401
+
+        ) {
+
+            qr()
+
+        }
+
+    })
+
+    XeonBotInc.ev.on('creds.update', saveCreds)
+
+    XeonBotInc.ev.on("messages.upsert",  () => { })
+
+}
+
+qr()
+
+
+
+process.on('uncaughtException', function (err) {
+
+let e = String(err)
+
+if (e.includes("Socket connection timeout")) return
+
+if (e.includes("rate-overlimit")) return
+
+if (e.includes("Connection Closed")) return
+
+if (e.includes("Timed Out")) return
+
+if (e.includes("Value not found")) return
+
+console.log('Caught exception: ', err)
+
+})
