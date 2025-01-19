4751d46b1b40769fe911ac95b88335172aa4f668
Xoul ser <131429508+sabariy@users.noreply.github.com>
2025-01-19 20:10:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "",
-  SESSION_NAME: process.env.SESSION_NAME || "Bot~"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://sabariyy:sabariyy@sabariyy.b85hd.mongodb.net/?retryWrites=true&w=majority&appName=Sabariyy",
+  SESSION_NAME: process.env.SESSION_NAME || "Fenix-Mf"
 };
